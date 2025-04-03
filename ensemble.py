d4d9b30e55f99313fce1bcff832ccfeccda235e6
test4373 <deneme2143y@gmail.com>
2025-04-04 02:18:27 +0300

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -6,20 +6,21 @@ import librosa
 import soundfile as sf
 import numpy as np
 import argparse
+import gc
 
 def stft(wave, nfft, hl):
     wave_left = np.asfortranarray(wave[0])
     wave_right = np.asfortranarray(wave[1])
-    spec_left = librosa.stft(wave_left, n_fft=nfft, hop_length=hl)
-    spec_right = librosa.stft(wave_right, n_fft=nfft, hop_length=hl)
+    spec_left = librosa.stft(wave_left, n_fft=nfft, hop_length=hl, window='hann')
+    spec_right = librosa.stft(wave_right, n_fft=nfft, hop_length=hl, window='hann')
     spec = np.asfortranarray([spec_left, spec_right])
     return spec
 
 def istft(spec, hl, length):
     spec_left = np.asfortranarray(spec[0])
     spec_right = np.asfortranarray(spec[1])
-    wave_left = librosa.istft(spec_left, hop_length=hl, length=length)
-    wave_right = librosa.istft(spec_right, hop_length=hl, length=length)
+    wave_left = librosa.istft(spec_left, hop_length=hl, length=length, window='hann')
+    wave_right = librosa.istft(spec_right, hop_length=hl, length=length, window='hann')
     wave = np.asfortranarray([wave_left, wave_right])
     return wave
 
@@ -63,17 +64,11 @@ def lambda_min(arr, axis=None, key=None, keepdims=False):
     else:
         return arr.flatten()[idxs]
 
-def average_waveforms(pred_track, weights, algorithm):
-    """
-    :param pred_track: shape = (num, channels, length)
-    :param weights: shape = (num, )
-    :param algorithm: One of avg_wave, median_wave, min_wave, max_wave, avg_fft, median_fft, min_fft, max_fft
-    :return: averaged waveform in shape (channels, length)
-    """
+def average_waveforms(pred_track, weights, algorithm, chunk_length):
     pred_track = np.array(pred_track)
-    final_length = pred_track.shape[-1]
-
+    pred_track = np.array([p[:, :chunk_length] if p.shape[1] > chunk_length else np.pad(p, ((0, 0), (0, chunk_length - p.shape[1])), 'constant') for p in pred_track])
     mod_track = []
+    
     for i in range(pred_track.shape[0]):
         if algorithm == 'avg_wave':
             mod_track.append(pred_track[i] * weights[i])
@@ -81,34 +76,35 @@ def average_waveforms(pred_track, weights, algorithm):
             mod_track.append(pred_track[i])
         elif algorithm in ['avg_fft', 'min_fft', 'max_fft', 'median_fft']:
             spec = stft(pred_track[i], nfft=2048, hl=1024)
-            if algorithm in ['avg_fft']:
+            if algorithm == 'avg_fft':
                 mod_track.append(spec * weights[i])
             else:
                 mod_track.append(spec)
     pred_track = np.array(mod_track)
 
-    if algorithm in ['avg_wave']:
-        pred_track = pred_track.sum(axis=0)
-        pred_track /= np.array(weights).sum().T
-    elif algorithm in ['median_wave']:
-        pred_track = np.median(pred_track, axis=0)
-    elif algorithm in ['min_wave']:
-        pred_track = lambda_min(pred_track, axis=0, key=np.abs)
-    elif algorithm in ['max_wave']:
-        pred_track = lambda_max(pred_track, axis=0, key=np.abs)
-    elif algorithm in ['avg_fft']:
+    if algorithm == 'avg_wave':
         pred_track = pred_track.sum(axis=0)
         pred_track /= np.array(weights).sum()
-        pred_track = istft(pred_track, 1024, final_length)
-    elif algorithm in ['min_fft']:
-        pred_track = lambda_min(pred_track, axis=0, key=np.abs)
-        pred_track = istft(pred_track, 1024, final_length)
-    elif algorithm in ['max_fft']:
-        pred_track = absmax(pred_track, axis=0)
-        pred_track = istft(pred_track, 1024, final_length)
-    elif algorithm in ['median_fft']:
+    elif algorithm == 'median_wave':
         pred_track = np.median(pred_track, axis=0)
-        pred_track = istft(pred_track, 1024, final_length)
+    elif algorithm == 'min_wave':
+        pred_track = lambda_min(pred_track, axis=0, key=np.abs)
+    elif algorithm == 'max_wave':
+        pred_track = lambda_max(pred_track, axis=0, key=np.abs)
+    elif algorithm == 'avg_fft':
+        pred_track = pred_track.sum(axis=0)
+        pred_track /= np.array(weights).sum()
+        pred_track = istft(pred_track, 1024, chunk_length)
+    elif algorithm == 'min_fft':
+        pred_track = lambda_min(pred_track, axis=0, key=np.abs)
+        pred_track = istft(pred_track, 1024, chunk_length)
+    elif algorithm == 'max_fft':
+        pred_track = absmax(pred_track, axis=0)
+        pred_track = istft(pred_track, 1024, chunk_length)
+    elif algorithm == 'median_fft':
+        pred_track = np.median(pred_track, axis=0)
+        pred_track = istft(pred_track, 1024, chunk_length)
+
     return pred_track
 
 def ensemble_files(args):
@@ -117,32 +113,75 @@ def ensemble_files(args):
     parser.add_argument("--type", type=str, default='avg_wave', help="One of avg_wave, median_wave, min_wave, max_wave, avg_fft, median_fft, min_fft, max_fft")
     parser.add_argument("--weights", type=float, nargs='+', help="Weights to create ensemble. Number of weights must be equal to number of files")
     parser.add_argument("--output", default="res.wav", type=str, help="Path to wav file where ensemble result will be stored")
-    args = parser.parse_args(args) if isinstance(args, list) else parser.parse_args()
+    if args is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(args)
 
     print('Ensemble type: {}'.format(args.type))
     print('Number of input files: {}'.format(len(args.files)))
     if args.weights is not None:
-        weights = args.weights
+        weights = np.array(args.weights)
     else:
         weights = np.ones(len(args.files))
     print('Weights: {}'.format(weights))
     print('Output file: {}'.format(args.output))
-    
-    data = []
-    for f in args.files:
-        if not os.path.isfile(f):
-            print('Error. Can\'t find file: {}. Check paths.'.format(f))
-            return None
-        print('Reading file: {}'.format(f))
-        wav, sr = librosa.load(f, sr=None, mono=False)
-        print("Waveform shape: {} sample rate: {}".format(wav.shape, sr))
-        data.append(wav)
-    
-    data = np.array(data)
-    res = average_waveforms(data, weights, args.type)
-    print('Result shape: {}'.format(res.shape))
-    sf.write(args.output, res.T, sr, 'FLOAT')
-    return args.output
+
+    durations = [librosa.get_duration(filename=f) for f in args.files]
+    if not all(d == durations[0] for d in durations):
+        raise ValueError("All files must have the same duration")
+
+    total_duration = durations[0]
+    sr = librosa.get_samplerate(args.files[0])
+    chunk_duration = 30  # 30 saniyelik parçalar
+    overlap_duration = 0.1  # 100 ms overlap
+    chunk_samples = int(chunk_duration * sr)
+    overlap_samples = int(overlap_duration * sr)
+    step_samples = chunk_samples - overlap_samples  # Adım boyutu overlap ile azaltılır
+    total_samples = int(total_duration * sr)
+
+    # Chunk uzunluğunu hop_length ile hizala
+    hop_length = 1024
+    chunk_samples = ((chunk_samples + hop_length - 1) // hop_length) * hop_length
+    step_samples = chunk_samples - overlap_samples
+
+    with sf.SoundFile(args.output, 'w', sr, channels=2, subtype='FLOAT') as outfile:
+        for start in range(0, total_samples, step_samples):
+            end = min(start + chunk_samples, total_samples)
+            chunk_length = end - start
+            data = []
+
+            for f in args.files:
+                if not os.path.isfile(f):
+                    print('Error. Can\'t find file: {}. Check paths.'.format(f))
+                    exit()
+                print(f'Reading chunk from file: {f} (start: {start/sr}s, duration: {(end-start)/sr}s)')
+                wav, _ = librosa.load(f, sr=sr, mono=False, offset=start/sr, duration=(end-start)/sr)
+                data.append(wav)
+
+            res = average_waveforms(data, weights, args.type, chunk_length)
+            res = res.astype(np.float32)
+            print(f'Chunk result shape: {res.shape}')
+
+            # Crossfade ile birleştirme
+            if start > 0:
+                outfile.seek(outfile.tell() - overlap_samples)
+                prev_data = outfile.read(overlap_samples).T
+                new_data = res[:, :overlap_samples]
+                fade_out = np.linspace(1, 0, overlap_samples)
+                fade_in = np.linspace(0, 1, overlap_samples)
+                blended = prev_data * fade_out + new_data * fade_in
+                outfile.seek(outfile.tell() - overlap_samples)
+                outfile.write(blended.T)
+                outfile.write(res[:, overlap_samples:].T)
+            else:
+                outfile.write(res.T)
+
+            del data
+            del res
+            gc.collect()
+
+    print(f'Ensemble completed. Output saved to: {args.output}')
 
 if __name__ == "__main__":
     ensemble_files(None)
